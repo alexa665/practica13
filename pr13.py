3425abdd690942bcad9be0e62fdd5859ad75bd21
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-24 20:51:08 -0600

    Add files via upload

---
+++ pr13.py
@@ -0,0 +1,9 @@
+print(" ")
+print("alexa guadalupe ramirez manzo")
+print(" ")
+j = 5 #variable y su valor
+numero = int(input("ingresa un numero")) #linea para ingresar un numero
+if (j == numero): #linea para la verificasion del numero
+    print("el numero es correcto") #impresion de la verificasion del numero
+else: #linea para verificar si el numero es falso o verdadero
+    print("el numero es incorrecto") #impresion de la verificasion del numero
